24be1833d7eb6655a72402355bdf6d4aa8590c8f
Zeke <jkezekiel@gmail.com>
2023-07-16 22:47:12 -0500

    adding code block capabiltites to blog

--- gridsome.config.js
+++ gridsome.config.js
@@ -7,13 +7,18 @@ const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: './zeke.sh',
   plugins: [
     {
       use: '@gridsome/source-filesystem',
       options: {
         typeName: 'BlogPost',
         path: './content/blog/**/*.md',
+        remark: {
+          plugins: [
+            ['gridsome-plugin-remark-shiki', { theme: 'nord', skipInline: true }],
+          ]
+        }
       }
     }
 
